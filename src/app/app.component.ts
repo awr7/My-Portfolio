674edae4fbf1849a15314f04a3af37962a254646
Angel <rosarioangel396@gmail.com>
2023-12-26 15:40:11 -0500

    created about me component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, WelcomeComponent],
+  imports: [CommonModule, RouterOutlet, WelcomeComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
