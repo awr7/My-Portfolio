1adaead5813dcb101815981a4940125c048a687a
Angel <rosarioangel396@gmail.com>
2024-01-01 22:48:41 -0500

    aded case studys component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProjectsComponent } from './projects/projects.component';
+import { CaseStudysComponent } from './case-studys/case-studys.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, WelcomeComponent, AboutMeComponent, HttpClientModule, ProjectsComponent],
+  imports: [CommonModule, RouterOutlet, WelcomeComponent, AboutMeComponent, HttpClientModule, ProjectsComponent, CaseStudysComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/case-studys/case-studys.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-studys',
+  standalone: true,
+  imports: [],
+  templateUrl: './case-studys.component.html',
+  styleUrl: './case-studys.component.scss'
+})
+export class CaseStudysComponent {
+
+}
