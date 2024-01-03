f846b3d43704c39919a6a84ac81812ab12254628
Angel <rosarioangel396@gmail.com>
2024-01-03 14:21:56 -0500

    styled contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor() {
+    this.contactForm = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'message': new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
 }
