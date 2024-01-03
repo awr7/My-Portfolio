59139f34e76951b5d2064ab89ec48f1c4ee0deb6
Angel <rosarioangel396@gmail.com>
2024-01-03 15:08:49 -0500

    added contact functionality

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import emailjs, { init } from 'emailjs-com';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
   contactForm: FormGroup;
+  showSuccessMessage: boolean = false;
+  showErrorMessage: boolean = false;
 
   constructor() {
+    emailjs.init("EoajKBrXJbuP671Ij");
+    
     this.contactForm = new FormGroup({
       'name': new FormControl(null, Validators.required),
       'email': new FormControl(null, [Validators.required, Validators.email]),
@@ -21,6 +27,27 @@ export class ContactComponent {
   }
 
   onSubmit() {
-    console.log(this.contactForm.value);
+    const templateParams = {
+      to_name: 'Angel',
+      from_name: this.contactForm.value.name,
+      from_email: this.contactForm.value.email,
+      message: this.contactForm.value.message,
+    };
+  
+    // Send the email
+    emailjs.send("service_u5jqczl", "template_2ks5v6k", templateParams)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        this.showSuccessMessage = true;
+      }, (error) => {
+        console.log('FAILED...', error);
+        this.showErrorMessage = true;
+      });
+  }  
+
+  closeMessage() {
+    this.showSuccessMessage = false;
+    this.showErrorMessage = false;
   }
+
 }
