f930c66e1847056f26574b2b972b1c2bd2a6b70a
Angel <rosarioangel396@gmail.com>
2024-01-06 11:28:12 -0500

    added functionality to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProjectsComponent } from './projects/projects.component';
 import { CaseStudysComponent } from './case-studies/case-studies.component';
 import { ContactComponent } from './contact/contact.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,15 @@ export class AppComponent {
   title = 'Portfolio';
   public isOpen = false;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+      
+  }
+  constructor(
+    private el: ElementRef, 
+    private renderer: Renderer2, 
+    private router: Router // Inject the Angular Router
+  ) {}
 
   toggleMenu(): void {
     this.isOpen = !this.isOpen;
@@ -49,5 +58,42 @@ export class AppComponent {
     this.isOpen = false;
     const pageElement = this.el.nativeElement.querySelector('.page');
     this.renderer.removeClass(pageElement, 'shazam');
+  }
+
+  scrollToElement(elementId: string, event: MouseEvent): void {
+    event.preventDefault(); // Prevent default anchor behavior
+    this.closeMenu();
+  
+    setTimeout(() => {
+      const element = document.getElementById(elementId);
+      if (element) {
+        const targetPosition = element.offsetTop;
+        const startPosition = window.scrollY;
+        const distance = targetPosition - startPosition;
+        const duration = 800; // Duration of the scroll animation in milliseconds
+        let startTime: number | null = null;
+  
+        const easeInOutQuad = (currentTime: number, startValue: number, changeInValue: number, duration: number) => {
+          currentTime /= duration / 2;
+          if (currentTime < 1) return (changeInValue / 2) * currentTime * currentTime + startValue;
+          currentTime--;
+          return (-changeInValue / 2) * (currentTime * (currentTime - 2) - 1) + startValue;
+        };
+  
+        const animateScroll = (currentTime: number) => {
+          if (!startTime) startTime = currentTime;
+          const timeElapsed = currentTime - startTime;
+          const nextScrollY = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+          window.scrollTo(0, nextScrollY);
+          if (timeElapsed < duration) {
+            requestAnimationFrame(animateScroll);
+          }
+        };
+  
+        requestAnimationFrame(animateScroll);
+      } else {
+        console.error('Element with ID ' + elementId + ' not found!');
+      }
+    }, 350); // Delay to allow the menu to close
   }
 }
